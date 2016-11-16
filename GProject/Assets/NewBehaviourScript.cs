ef4d89ae2d30c9ded8a5e03c470c56431e24ece7
Łukasz <mertalukasz@gmail.com>
2016-11-16 20:47:27 +0100

    Dodanie podstawowego projektu unity

---
+++ GProject/Assets/NewBehaviourScript.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    protected int speed = 0;
+    protected Camera mMainCamera;
+
+    void squareLogic()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            speed += 1;
+        }
+        else if (Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            speed = 0;
+        }
+
+        transform.Translate(0, 0, speed * Time.deltaTime);
+    }
+
+    void doCameraMovement()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            mMainCamera.transform.Translate(0.0f, 0.0f, 13.0f * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            mMainCamera.transform.Translate(0.0f , 0.0f, -13.0f * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            mMainCamera.transform.Translate(13.0f * Time.deltaTime, 0.0f, 0.0f);
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            mMainCamera.transform.Translate(-13.0f * Time.deltaTime, 0.0f, 0.0f);
+        }
+        else if (Input.GetKey(KeyCode.Space))
+        {
+            mMainCamera.transform.Translate(13.0f * Time.deltaTime, 0.0f, 0.0f);
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+        mMainCamera = Camera.main;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        squareLogic();
+        doCameraMovement();
+    }
+}
